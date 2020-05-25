38ff8455f55efc2d64e4836e0ce079fd98333db8
Barak Roth <brk.rot@gmail.com>
2020-05-25 09:48:33 +0300

    Commit #3 Half way with main menu

--- app/src/main/java/com/example/alshelper/MainActivity.java
+++ app/src/main/java/com/example/alshelper/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.alshelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void move(View v){
+        Intent intent = new
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
